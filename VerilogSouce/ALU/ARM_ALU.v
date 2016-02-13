3f9d2f7dba56f34d496456f7b4a3b6106fb17475
Apo45ty <antonio.tapia@upr.edu>
2016-02-13 11:31:41 -0400

    Added ALU Debug

--- VerilogSouce/ALU/ARM_ALU.v
+++ VerilogSouce/ALU/ARM_ALU.v
@@ -1,9 +1,8 @@
 module ARM_ALU(input wire [31:0] A,B,input wire[4:0] OP,input wire [3:0] FLAGS,output wire [31:0] Out,output wire [3:0] FLAGS_OUT, input wire S,ALU_OUT);
 
-
 parameter HIGHZ = 32'hzzzzzzzz;
-reg [31:0] buffer=32'h00000000;
-reg [3:0] FLAGS_buff=4'h0;
+reg [31:0] buffer;
+reg [3:0] FLAGS_buff;
 
 /*
 STATUS REGISTER FLAGS
@@ -29,9 +28,9 @@ casez(OP)
   5'b00001,5'b01001:buffer <= A ^ B ;
   //Sub Substract, Out = A - B
   //CMP, perform an SUB but dont transfer it to a register
-  5'b00010,5'b01010:buffer <= A - B ;
+  5'b00010,5'b01010:{FLAGS_buff[1],buffer} <= A - B ;
   //RSB, OUT = B - A
-  5'b00011:buffer <= B - A;
+  5'b00011:{FLAGS_buff[1],buffer} <= B - A;
   //ADD, OUT = A + B
   //CMN, perform an ADD but dont transfer it to a register
   5'b00100,5'b01011:
@@ -39,24 +38,25 @@ casez(OP)
 		{FLAGS_buff[1],buffer} <= A + B ;
 	  end
   //ADC, OUT = A + B + C
-  5'b00101:buffer <= A + B + FLAGS[1];
+  5'b00101:{FLAGS_buff[1],buffer} <= A + B + FLAGS[1];
   //SBC, OUT = A-B-(~C)
-  5'b00110:buffer <= A - B - !FLAGS[1];
+  5'b00110:{FLAGS_buff[1],buffer} <= A - B - !FLAGS[1];
   //RSC, OUT = B-A-(~C)
-  5'b00111:buffer <= B - A - !FLAGS[1];
+  5'b00111:{FLAGS_buff[1],buffer} <= B - A - !FLAGS[1];
   //ORR, OUT = A or B
   5'b01100:buffer <= A | B;
   //BIC, OUT = A or not(B)
   5'b01110:buffer <= A & !B;
+  //MVN Rd := NOT shifter_operand (no first operand)
+  5'b01111:buffer <= !B;
   //BYPASS
-  5'bzzzzz, 5'b10000:buffer <= B;
+  5'b10000:buffer <= B;
   //add 4 A
   5'b10001:buffer <= A + 1;
-  default: buffer <= B;
 endcase
 end
   
-always@(buffer)
+always@(buffer or A or B or OP )
 begin
   FLAGS_buff[3] = buffer[31];
   FLAGS_buff[2] = buffer == 32'h0;
@@ -64,6 +64,6 @@ begin
 end
 
 assign FLAGS_OUT = S ? FLAGS_buff : FLAGS;
-assign Out =   buffer ;
+assign Out =  ALU_OUT ?  buffer : HIGHZ;
 
 endmodule
