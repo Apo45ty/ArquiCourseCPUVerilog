6e585e6bb06e777301931684c20a8ee6b3ae746e
Apo45ty <antonio.tapia@upr.edu>
2016-02-14 15:22:18 -0400

    Finalized tests on BarrelShifter and created test.txt to save correct console output.

--- VerilogSouce/BarrelShifter/BarrelShifter.v
+++ VerilogSouce/BarrelShifter/BarrelShifter.v
@@ -64,7 +64,7 @@ begin
 				else begin
 				temp = {SR29_IN,shiftVal };
 				temp2 = 0;				
-				for(i = 0; i <= 32- shiftAmount[4:0] ;i = i + 1) begin
+				for(i = 0; i <= 32 - shiftAmount[4:0] ;i = i + 1) begin
 						temp2[32-i] = temp[32-i-shiftAmount[4:0] ] ; 
 				end
 				{_SR29_OUT,_Out} = temp2; 
@@ -78,7 +78,7 @@ begin
 				else if (shiftAmount>=33)
 					{_SR29_OUT,_Out} = 0 ;
 				else begin
-				temp = {SR29_IN,shiftVal };
+				temp = {shiftVal,SR29_IN};
 				temp2 = 0;				
 				for(i = 0; i <= 32-shiftAmount[4:0] ;i = i + 1) begin
 						temp2[i] = temp[i+shiftAmount[4:0] ] ; 
@@ -90,31 +90,35 @@ begin
 			begin 
 				$display("ASR");
 				if(shiftAmount==32)
-					_Out = {SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN};	
+					begin
+						_Out = {shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31]};	
+					end
 				else if (shiftAmount>=33)
-					{_SR29_OUT,_Out} = {SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN};	
+					begin
+						{_SR29_OUT,_Out} = {shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+							 shiftVal[31],shiftVal[31]};
+					end
 				else begin
-				temp = {SR29_IN,shiftVal };
+				temp = {shiftVal,SR29_IN };
 				//Lazy to encode in loop
-				temp2 = {SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN,SR29_IN,SR29_IN,SR29_IN,
-						 SR29_IN,SR29_IN};				
+				temp2 = {shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+						 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+						 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+						 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+						 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+						 shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],shiftVal[31],
+						 shiftVal[31],shiftVal[31]};				
 				for(i = 0; i <= 32-shiftAmount[4:0] ;i = i + 1) begin
 						temp2[i] = temp[i+shiftAmount[4:0] ] ; 
 				end
@@ -124,10 +128,11 @@ begin
 		2'b11:
 			begin
 				$display("ROR");
-				temp = {SR29_IN,shiftVal };
+				temp = {shiftVal,SR29_IN};
 				temp2 = temp;				
 				for(i = 0; i <= 32 ;i = i + 1) begin
-						temp2[i] = temp[(i+shiftAmount[4:0])%32] ; 
+						temp2[i] = temp[(i+shiftAmount[4:0])%33] ; 
+						$display("[%3d]=[%3d]",i,(i+shiftAmount[4:0])%33);
 				end
 				{_Out,_SR29_OUT} = temp2; 
 			end
--- VerilogSouce/BarrelShifter/BarrelShifterTestBench.v
+++ VerilogSouce/BarrelShifter/BarrelShifterTestBench.v
@@ -12,11 +12,26 @@ BarrelShifter bs(Rs,Rm,IR,SR29_IN,SR29_OUT,Out);
 initial fork
 	Rs=0;Rm=0;IR=0;SR29_IN=0;
 	#1 Rs=1;#1 Rm=2;#1 IR[4]=1;#1 SR29_IN=0;
-	#2 Rs=1;#2 Rm=2;#2 IR[6:5]=1;#2 SR29_IN=0;
-	#3 Rs=1;#3 Rm=2;#3 IR[6:5]=2;#3 SR29_IN=0;
-	#4 Rs=1;#4 Rm=2;#4 IR[6:5]=3;#4 SR29_IN=0;
-	#5 Rs=0;#5 Rm=0;#5 IR[27:25]=1;#5 IR[4]=0;#5 SR29_IN=0;
-	#6 Rs=0;#6 Rm=0;#6 IR[27:25]=3'b101;#6 SR29_IN=0;
+	#2 Rs=1;#2 Rm=4; #2 SR29_IN=0;
+	#3 Rs=1;#3 Rm=8; #3 SR29_IN=0;
+	#4 Rs=8;#4 Rm=1; #4 SR29_IN=0;
+	#10 Rs=1;#10 Rm=2;#10 IR[6:5]=1;#10 SR29_IN=0;
+	#11 Rs=1;#11 Rm=4; #11 SR29_IN=0;
+	#12 Rs=1;#12 Rm=8; #12 SR29_IN=0;
+	#13 Rs=8;#13 Rm=1; #13 SR29_IN=0;
+	#20 Rs=1;#20 Rm=2;#20 IR[6:5]=2;#20 SR29_IN=0;
+	#21 Rs=1;#21 Rm=4; #21 SR29_IN=0;
+	#22 Rs=1;#22 Rm=8; #22 SR29_IN=0;
+	#23 Rs=8;#23 Rm=1; #23 SR29_IN=0;
+	#24 Rs=8;#24 Rm=32'hF0000001; #24 SR29_IN=0;
+	#30 Rs=1;#30 Rm=2;#30 IR[6:5]=3;#30 SR29_IN=0;
+	#31 Rs=1;#31 Rm=4; #31 SR29_IN=0;
+	#32 Rs=1;#32 Rm=8; #32 SR29_IN=0;
+	#33 Rs=8;#33 Rm=1; #33 SR29_IN=0;
+	#34 Rs=8;#34 Rm=32'hF0000001; #34 SR29_IN=0;
+	#40 IR[11:8]=0;#40 IR[7:0]=0;#40 IR[27:25]=1;#40 IR[4]=0;#40 SR29_IN=0;
+	#41 IR[11:8]=8;#41 IR[7:0] =1 ; #41 SR29_IN=0;
+	#50 Rs=0;#50 Rm=0;#50 IR[27:25]=3'b101;#50 SR29_IN=0;
 join
 	
 initial #sim_time $finish; 
@@ -30,3 +45,4 @@ end
 
 endmodule
 //iverilog BarrelShifter.v BarrelShifterTestBench.v
+//reference 1 http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0497a/CIHDDCIF.html
