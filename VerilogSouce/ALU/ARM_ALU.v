75cadf57722eb850c9302234fa0cca64a62886cc
Apo45ty <antonio.tapia@upr.edu>
2016-02-14 16:11:20 -0400

    BarrelShifterFinished and Alu Fixed, Signal Enumaration Started

--- VerilogSouce/ALU/ARM_ALU.v
+++ VerilogSouce/ALU/ARM_ALU.v
@@ -1,7 +1,7 @@
 module ARM_ALU(input wire [31:0] A,B,input wire[4:0] OP,input wire [3:0] FLAGS,output wire [31:0] Out,output wire [3:0] FLAGS_OUT, input wire S,ALU_OUT);
 
 parameter HIGHZ = 32'hzzzzzzzz;
-reg [31:0] buffer;
+reg [31:0] buffer,_A,_B;
 reg [3:0] FLAGS_buff;
 
 /*
@@ -16,6 +16,8 @@ END
 always @(A or B or OP)
 begin
 FLAGS_buff=4'h0;
+_A=A;
+_B=B;
 casez(OP)
   //AND, Out = A & B
   //TST, perform an AND but dont transfer it to a register
@@ -28,9 +30,17 @@ casez(OP)
   5'b00001,5'b01001:buffer <= A ^ B ;
   //Sub Substract, Out = A - B
   //CMP, perform an SUB but dont transfer it to a register
-  5'b00010,5'b01010:{FLAGS_buff[1],buffer} <= A - B ;
+  5'b00010,5'b01010:
+	begin 
+		_B = ~B+1;
+		{FLAGS_buff[1],buffer} <= _A + _B ;
+	end
   //RSB, OUT = B - A
-  5'b00011:{FLAGS_buff[1],buffer} <= B - A;
+  5'b00011:
+	begin 
+		_A = ~A+1;
+		{FLAGS_buff[1],buffer} <= _B + _A;
+	end
   //ADD, OUT = A + B
   //CMN, perform an ADD but dont transfer it to a register
   5'b00100,5'b01011:
@@ -40,9 +50,17 @@ casez(OP)
   //ADC, OUT = A + B + C
   5'b00101:{FLAGS_buff[1],buffer} <= A + B + FLAGS[1];
   //SBC, OUT = A-B-(~C)
-  5'b00110:{FLAGS_buff[1],buffer} <= A - B - !FLAGS[1];
+  5'b00110:
+	begin 
+		_B = ~B+1;
+		{FLAGS_buff[1],buffer} <= _A + _B - !FLAGS[1];
+	end
   //RSC, OUT = B-A-(~C)
-  5'b00111:{FLAGS_buff[1],buffer} <= B - A - !FLAGS[1];
+  5'b00111:
+	begin 
+		_A = ~A+1;
+		{FLAGS_buff[1],buffer} <= B + A - !FLAGS[1];
+	end
   //ORR, OUT = A or B
   5'b01100:buffer <= A | B;
   //BIC, OUT = A or not(B)
@@ -64,11 +82,11 @@ casez(OP)
 endcase
 end
   
-always@(buffer or A or B or OP )
+always@(buffer or _A or _B or OP )
 begin
   FLAGS_buff[3] = buffer[31];
   FLAGS_buff[2] = buffer == 32'h0;
-  FLAGS_buff[0] = A[31] == B[31] && A[31] != buffer[31];
+  FLAGS_buff[0] = _A[31] == _B[31] && _A[31] != buffer[31];
 end
 
 assign FLAGS_OUT = S ? FLAGS_buff : FLAGS;
